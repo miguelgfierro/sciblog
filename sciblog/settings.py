a5f82a94af40a2bb1f700ff9326556c818f27da6
hoaphumanoid <miguelgfierro@gmail.com>
2015-11-27 11:31:23 +0100

    added social icons

--- sciblog/settings.py
+++ sciblog/settings.py
@@ -105,12 +105,8 @@ STATIC_URL = '/static/'
 # Template directory
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
-'''
-TEMPLATE_LOADERS = (
-    ('django.template.loaders.cached.Loader', (
-        'django.template.loaders.app_directories.Loader',
-        'django.template.loaders.filesystem.Loader',
-    )),
-)
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-'''
+#Social sharing settings. Fist log on https://www.addthis.com and get an ID
+# code from https://github.com/rwanyoike/django-addthis
+
+
+
