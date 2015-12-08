bc3d554e5407fbc6fc45b5e463f9423ecebd93c4
hoaphumanoid <miguelgfierro@gmail.com>
2015-12-08 10:02:18 +0100

    added templatetag current site

---
+++ blog/templatetags/current_site.py
@@ -0,0 +1,16 @@
+from django import template
+from django.contrib.sites.models import Site
+register = template.Library()
+
+@register.simple_tag
+def current_site():
+    """
+        Returns the url for the current site.
+    """
+    try:
+        current_site = Site.objects.get_current()
+        print 'site=', current_site.domain
+    except:
+        return "Problem when accessing object"
+
+    return current_site.domain
