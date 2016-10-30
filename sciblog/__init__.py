3d6a5da351cb8f8fd7350e0af2d2291a484a5f11
hoaphumanoid <miguelgfierro@gmail.com>
2016-10-30 08:41:17 +0000

    changed to version 2.5

--- sciblog/__init__.py
+++ sciblog/__init__.py
@@ -1,6 +1,6 @@
 
 __title__ = 'Sciblog - A blog designed like a scientific Latex paper'
-__version__ = '2.4'
+__version__ = '2.5'
 __author__ = 'Miguel Gonzalez-Fierro'
 __license__ = 'MIT license'
 __copyright__ = 'Copyright 2015-2016 Miguel Gonzalez-Fierro'
