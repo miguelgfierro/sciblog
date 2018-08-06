0748d66e4ce33fa83ab362617efe402544669320
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-08-06 07:28:21 +0100

    initial script for pausing and resuming cloudflare #21

---
+++ cron_ssl_renew.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from sciblog.private import CLOUDFLARE_ZONEID, CLOUDFLARE_APIKEY
+from sciblog.settings import EMAIL_ADDRESS
+
+
+# Variables
+HEADERS = {
+    'Content-type': 'application/json',
+    'X-Auth-Email': EMAIL_ADDRESS,
+    'X-Auth-Key': CLOUDFLARE_APIKEY
+}
+
+ENDPOINT = 'https://api.cloudflare.com/client/v4/zones/' + CLOUDFLARE_ZONEID
+
+
+def pause_cloudflare():
+    data = {'paused': 'true'}
+    res = requests.post(ENDPOINT,
+                        data=json.dumps(data), headers=HEADERS)
+    if res.ok:
+        print("Cloudflare paused")
+    else:
+        print("ERROR when pausing Cloudflare")
+        print(res.json())
+
+
+def resume_cloudflare():
+    data = {'paused': 'false'}
+    res = requests.post(ENDPOINT,
+                        data=json.dumps(data), headers=HEADERS)
+    if res.ok:
+        print("Cloudflare resumed")
+    else:
+        print("ERROR when resuming Cloudflare")
+        print(res.json())
+
+
+pause_cloudflare()
