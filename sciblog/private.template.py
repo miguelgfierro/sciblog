e9e49332d57a7f332e499d9645e558c94bf8690f
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-08-06 07:04:04 +0100

    adding cloudflare api info

--- sciblog/private.template.py
+++ sciblog/private.template.py
@@ -1,3 +1,9 @@
+# Django settings
 SECRETKEY = '7@ug(w91q62^z^vf3fcs$95+@&18m8vj#+of03q5#058axfd_8'
 DEBUG_FLAG = False
 
+# Cloudflare settings
+# The ZoneID can be obtained in the main dashboard under domain summary
+# The API Key can be accessed in the profile https://dash.cloudflare.com/profile
+CLOUDFLARE_ZONEID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+CLOUDFLARE_APIKEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
