fdf6296f726ecfec54a2e75ccfea4d596bb83671
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-10-21 09:03:14 +0200

    updated to version 2.11

--- sciblog/__init__.py
+++ sciblog/__init__.py
@@ -1,9 +1,9 @@
 
-__title__ = 'Sciblog - A blog designed like a scientific Latex paper'
-__version__ = '2.10'
-__author__ = 'Miguel Gonzalez-Fierro'
-__license__ = 'BSD License'
-__copyright__ = 'Copyright 2015-present Miguel Gonzalez-Fierro'
+__title__ = "Sciblog - A blog designed like a scientific Latex paper"
+__version__ = "2.11"
+__author__ = "Miguel Gonzalez-Fierro"
+__license__ = "BSD License"
+__copyright__ = "Copyright 2015-present Miguel Gonzalez-Fierro"
 
 # Version synonym
 VERSION = __version__
