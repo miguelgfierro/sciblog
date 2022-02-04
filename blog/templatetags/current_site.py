ec50cba893be2b8c706b9cc88204ec341d8d1a1a
miguelgfierro <miguelgfierro@users.noreply.github.com>
2022-02-04 18:58:59 +0100

    migration to python 3

--- blog/templatetags/current_site.py
+++ blog/templatetags/current_site.py
@@ -2,28 +2,29 @@ from django import template
 from django.contrib.sites.models import Site
 import sciblog.settings as scisettings
 
+
 register = template.Library()
+
 
 @register.simple_tag
 def current_site():
-    """
-        Returns the url for the current site defined by SITE_ID in sciblog/settings.py
-    """
+    """Returns the url for the current site defined by `SITE_ID` in `sciblog/settings.py`"""
     try:
         current_site = Site.objects.get_current()
     except:
-        print 'ERROR: Problem when accessing object'
+        print("ERROR: Problem when accessing object")
         return ""
 
     return current_site.domain
 
+
 @register.simple_tag
 def http_or_https():
-    """
-        Returns the http protocol. By default the protocol is http:// but if it's defined in settings, the protocol
-        can be https://
+    """Returns the http protocol. By default the protocol is `http://` but if it's 
+    defined in settings, the protocol can be `https://`.
     """
     if scisettings.HTTPS and not scisettings.DEBUG:
-        return 'https://'
+        return "https://"
     else:
-        return 'http://'
+        return "http://"
+
