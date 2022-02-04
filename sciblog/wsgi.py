c7f7812664bf9c4a1bfa12398506569f03e54a38
miguelgfierro <miguelgfierro@users.noreply.github.com>
2022-02-04 18:34:22 +0100

    refact

--- sciblog/wsgi.py
+++ sciblog/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sciblog.settings")
-
-from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
