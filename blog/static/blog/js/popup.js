b26b42b96f2e243251703467756e8149aae15860
miguelgfierro <miguelgfierro@users.noreply.github.com>
2022-07-20 23:25:17 +0200

    cookies

--- blog/static/blog/js/popup.js
+++ blog/static/blog/js/popup.js
@@ -1,13 +1,22 @@
 var lastFocus;
-var delay = 3000;
+var delay = 3000; // Pop up delay in seconds
+var cookieExpires = 1; // Expiration of cookie in days
+var popupShown = Cookies.get('popUpSciblog');
 
-setTimeout(function () {
-    lastFocus = document.activeElement;
-    console.log("Waiting for pop-up...");
-    $('#overlay').addClass('blur-in');
-    $('.pop-up').fadeIn(1000).css("display", "flex");
-}, delay);
-
+if (!!popupShown) {
+    console.log("No cookie found, showing pop up")
+    setTimeout(function () {
+        lastFocus = document.activeElement;
+        console.log("Waiting for pop-up...");
+        $('#overlay').addClass('blur-in');
+        $('.pop-up').fadeIn(1000).css("display", "flex");
+        Cookies.set('popUpSciblog', 'yes', {
+            expires: cookieExpires
+        });
+    }, delay);
+} else {
+    console.log("Cookie found, no pop up shown")
+}
 
 $('.close-button').click(function (e) {
     $('.pop-up').fadeOut(700);
