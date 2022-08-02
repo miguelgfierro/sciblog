1aeb1cef6aedc001eb303b55b1993621a5e86b86
miguelgfierro <miguelgfierro@users.noreply.github.com>
2022-08-02 22:42:08 +0200

    tagmanager

--- blog/templatetags/analytics.py
+++ blog/templatetags/analytics.py
@@ -7,18 +7,34 @@ register = template.Library()
 @register.simple_tag
 def ga_tracking_id():
     """
-    Returns data from Google Analytics defined in settings.py
+    Returns data from Google Tag Manager defined in settings.py.
 
-    To load in the template:
+    For more information about Google Tag Manager, see https://tagmanager.google.com/
+
+    To load in the template, add in the header:
     {% load analytics %}
     ...
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id={% ga_tracking_id %}"></script>
+    <!-- Google Tag Manager -->
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '{% ga_tracking_id %}');
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+          new Date().getTime(), event: 'gtm.js'
+      }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', '{% ga_tracking_id %}');
     </script>
+    <!-- End Google Tag Manager -->
+
+    Then add in the body:
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id={% ga_tracking_id %}" height="0" width="0"
+        style="display:none;visibility:hidden">
+    </iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     """
     return scisettings.GA_TRACKING_ID
